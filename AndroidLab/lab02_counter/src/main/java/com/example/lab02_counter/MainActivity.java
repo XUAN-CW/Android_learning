f1135890b8f430da3d8a7a3e4b876b6c6929190a
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-01 13:49:57 +0800

    完成了实验1、2、3
    1.1 第一个 Android 项目——Hello World!
    1.2 第二个 Android 项目——计数器
    1.3 第三个 Android 项目——新闻阅读器

---
+++ AndroidLab/lab02_counter/src/main/java/com/example/lab02_counter/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab02_counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnShowToast = findViewById(R.id.btnShowToast);
+        btnShowToast.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "here is toast",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnCount = findViewById(R.id.btnCount);
+        final TextView tvCount = findViewById(R.id.tvTextView);
+        btnCount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                tvCount.setText(Integer.toString(++count));
+            }
+        });
+
+
+
+    }
+}
