cd5cfd8e14ebdb8451b9333076a317adae9dfa73
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-21 22:11:07 +0800

    完成了实验十三

--- AndroidLab2/Lab13_GGMusic4/app/src/main/java/com/example/lab13_ggmusic4/MainActivity.java
+++ AndroidLab2/Lab13_GGMusic4/app/src/main/java/com/example/lab13_ggmusic4/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.lab13_ggmusic4;
 
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +37,7 @@ import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -57,6 +68,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tvBottomArtist;
     private ImageView ivAlbumThumbnail;
 
+    private MusicService mService;
+    private boolean mBound = false;
+
+    private Boolean mPlayStatus = true;
+    private ImageView ivPlay;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +83,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mContentResolver = getContentResolver();
         mCursorAdapter = new MediaCursorAdapter(MainActivity.this);
         mPlaylist = findViewById(R.id.lv_playlist);
+
+        if (ivPlay != null) {
+            ivPlay.setOnClickListener(MainActivity.this);
+        }
+
+
         mPlaylist.setOnItemClickListener( new ListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView,
@@ -162,7 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         navigation,
                         true);
 
-        ImageView ivPlay = navigation.findViewById(R.id.iv_play);
+        ivPlay = navigation.findViewById(R.id.iv_play);
         tvBottomTitle = navigation.findViewById(R.id.tv_bottom_title);
         tvBottomArtist = navigation.findViewById(R.id.tv_bottom_artist);
         ivAlbumThumbnail = navigation.findViewById(R.id.iv_thumbnail);
@@ -173,7 +197,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         navigation.setVisibility(View.GONE);
 
-
+        musicReceiver = new MusicReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_MUSIC_START);
+        intentFilter.addAction(ACTION_MUSIC_STOP);
+        registerReceiver(musicReceiver , intentFilter);
+//        pbProgress=new ProgressBar(MainActivity.this);
+        pbProgress=findViewById(R.id.progress);
+        System.out.println("++++++++++++++++="+findViewById(R.id.progress).toString());
+        System.out.println("--------:"+pbProgress);
     }
 
     @Override
@@ -209,99 +241,150 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View view) {
+        if (view.getId() == R.id.iv_play) {
+            mPlayStatus = !mPlayStatus;
+            if (mPlayStatus == true) {
+                mService.play();
+                ivPlay.setImageResource(
+                        R.drawable.ic_pause_circle_outline_black_24dp);
+            } else {
+                mService.pause();
+                ivPlay.setImageResource(
+                        R.drawable.ic_play_circle_outline_black_24dp);
+            }
+        }
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if (mMediaPlayer == null) {
+//            mMediaPlayer = new MediaPlayer();
+//        }
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        if (mMediaPlayer != null) {
+//            mMediaPlayer.stop();
+//            mMediaPlayer.release();
+//            mMediaPlayer = null;
+////            Log.d(MainActivity.TAG, "onStop invoked!");
+//            System.out.println("onStop invoked!");
+//        }
+//        super.onStop();
+//    }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (mMediaPlayer == null) {
-            mMediaPlayer = new MediaPlayer();
-        }
+
+        Intent intent = new Intent(MainActivity.this ,
+                MusicService.class);
+        bindService(intent , mConn , Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onStop() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.stop();
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-//            Log.d(MainActivity.TAG, "onStop invoked!");
-            System.out.println("onStop invoked!");
-        }
+        unbindService(mConn);
+        mBound = false;
+
         super.onStop();
     }
 
-//    private ListView.OnItemClickListener itemClickListener = new ListView.OnItemClickListener() {
-//
-//        @Override
-//        public void onItemClick(AdapterView<?> adapterView,
-//                                View view, int i, long l) {
-//            System.out.println("itemClickListener");
-//            Cursor cursor = mCursorAdapter.getCursor();
-//            if (cursor != null && cursor.moveToPosition(i)) {
-//
-//                int titleIndex = cursor.getColumnIndex(
-//                        MediaStore.Audio.Media.TITLE);
-//                int artistIndex = cursor.getColumnIndex(
-//                        MediaStore.Audio.Media.ARTIST);
-//                int albumIdIndex = cursor.getColumnIndex(
-//                        MediaStore.Audio.Media.ALBUM_ID);
-//                int dataIndex = cursor.getColumnIndex(
-//                        MediaStore.Audio.Media.DATA);
-//
-//                String title = cursor.getString(titleIndex);
-//                String artist = cursor.getString(artistIndex);
-//                Long albumId = cursor.getLong(albumIdIndex);
-//                String data = cursor.getString(dataIndex);
-//                Uri dataUri = Uri.parse(data);
-//
-//                if (mMediaPlayer != null) {
-//                    try {
-//                        mMediaPlayer.reset();
-//                        mMediaPlayer.setDataSource(
-//                                MainActivity.this, dataUri);
-//                        mMediaPlayer.prepare();
-//                        mMediaPlayer.start();
-//                    } catch (IOException ex) {
-//                        ex.printStackTrace();
-//                    }
-//                }
-//
-//                navigation.setVisibility(View.VISIBLE);
-//
-//                if (tvBottomTitle != null) {
-//                    tvBottomTitle.setText(title);
-//                }
-//                if (tvBottomArtist != null) {
-//                    tvBottomArtist.setText(artist);
-//                }
-//
-//                Uri albumUri = ContentUris.withAppendedId(
-//                        MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
-//                        albumId);
-//
-//                Cursor albumCursor = mContentResolver.query(
-//                        albumUri,
-//                        null,
-//                        null,
-//                        null,
-//                        null);
-//
-//                if (albumCursor != null && albumCursor.getCount() > 0) {
-//                    albumCursor.moveToFirst();
-//                    int albumArtIndex = albumCursor.getColumnIndex(
-//                            MediaStore.Audio.Albums.ALBUM_ART);
-//                    String albumArt = albumCursor.getString(
-//                            albumArtIndex);
-//                    Glide.with(MainActivity.this)
-//                            .load(albumArt)
-//                            .into(ivAlbumThumbnail);
-//                    albumCursor.close();
-//                }
-//            }
-//        }
-//    };
+    private ServiceConnection mConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(
+                ComponentName componentName , IBinder iBinder) {
+            MusicService.MusicServiceBinder binder =
+                    (MusicService.MusicServiceBinder) iBinder;
+
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(
+                ComponentName componentName) {
+            mService = null;
+            mBound = false;
+        }
+    };
+
+
+    public static final int UPDATE_PROGRESS = 1;
+    private ProgressBar pbProgress;
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_PROGRESS:
+                    int position = msg.arg1;
+                    System.err.println(new Date().toString()+":"+position);
+                    pbProgress.setProgress(position);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
+    private class MusicProgressRunnable implements Runnable {
+        public MusicProgressRunnable() {
+        }
+
+        @Override
+        public void run() {
+            boolean mThreadWorking = true;
+            while (mThreadWorking) {
+                try {
+                    if (mService != null) {
+                        int position =
+                                mService.getCurrentPosition();
+                        Message message = new Message();
+                        message.what = UPDATE_PROGRESS;
+                        message.arg1 = position;
+                        mHandler.sendMessage(message);
+                    }
+                    mThreadWorking = mService.isPlaying();
+                    Thread.sleep(100);
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    public static final String ACTION_MUSIC_START =
+            "com.glriverside.xgqin.ggmusic.ACTION_MUSIC_START";
+    public static final String ACTION_MUSIC_STOP =
+            "com.glriverside.xgqin.ggmusic.ACTION_MUSIC_STOP";
+    private MusicReceiver musicReceiver;
+
+
+    public class MusicReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context , Intent intent) {
+
+            if (mService != null) {
+                System.out.println("-----------------pbProgress:"+pbProgress);
+                pbProgress.setMax(mService.getDuration());
+                new Thread(new MusicProgressRunnable()).start();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        unregisterReceiver(musicReceiver);
+        super.onDestroy();
+    }
+
 
 }
--- AndroidLab2/Lab13_GGMusic4/app/src/main/java/com/example/lab13_ggmusic4/MusicService.java
+++ AndroidLab2/Lab13_GGMusic4/app/src/main/java/com/example/lab13_ggmusic4/MusicService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
 
 import java.io.IOException;
@@ -11,12 +12,14 @@ import java.io.IOException;
 public class MusicService extends Service {
 
     MediaPlayer mMediaPlayer;
+    private final IBinder mBinder = new MusicServiceBinder();
 
-    public MusicService() {}
+    public MusicService() {
+    }
 
     @Override
-    public int onStartCommand(Intent intent ,
-                              int flags , int startId) {
+    public int onStartCommand(Intent intent,
+                              int flags, int startId) {
         String data = intent.getStringExtra(
                 MainActivity.DATA_URI);
         Uri dataUri = Uri.parse(data);
@@ -29,12 +32,19 @@ public class MusicService extends Service {
                         dataUri);
                 mMediaPlayer.prepare();
                 mMediaPlayer.start();
+
+
+                Intent musicStartIntent =
+                        new Intent(MainActivity.ACTION_MUSIC_START);
+                sendBroadcast(musicStartIntent);
+
+
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
 
-        return super.onStartCommand(intent , flags , startId);
+        return super.onStartCommand(intent, flags, startId);
     }
 
 
@@ -55,6 +65,57 @@ public class MusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+
+
+    public class MusicServiceBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    /** method for clients */
+    public void pause() {
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+        }
+    }
+
+    public void play() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.start();
+        }
+    }
+
+    public int getDuration() {
+        int duration = 0;
+
+        if (mMediaPlayer != null) {
+            duration = mMediaPlayer.getDuration();
+        }
+
+        return duration;
+    }
+    public int getCurrentPosition() {
+        int position = 0;
+
+        if (mMediaPlayer != null) {
+            position = mMediaPlayer.getCurrentPosition();
+        }
+
+        return position;
+    }
+
+    public boolean isPlaying() {
+
+        if (mMediaPlayer != null) {
+            return mMediaPlayer.isPlaying();
+        }
+        return false;
     }
 }
+
+
